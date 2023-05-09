46da148f6bd8a0eeb6fbd5850ade650d003d1ccc
Jeffrey <1115378579@qq.com>
2023-05-09 18:02:21 +0800

    wip: 优化操作

--- main.js
+++ main.js
@@ -4,17 +4,19 @@ const createWindow = () => {
   const win = new BrowserWindow({
     icon: './icon.png',
     width: 600,
-    height: 120,
+    height: 320,
     transparent: true,
     frame: false,
     alwaysOnTop: true,
+    x: 0,
+    y: 0,
 
   })
 
   win.loadFile('index.html')
   win.setIcon('./icon.png')
   // win.webContents.openDevTools({
-  //   mode: 'bottom'
+  //   mode: 'detach'
   // });
 }
 
